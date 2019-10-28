9b2ebad0f752ec41e83a3bf409523bbfd1bedd54
khainguyen24 <35777870+khainguyen24@users.noreply.github.com>
2019-10-28 16:01:18 -0400

    added Major_Counts to the first line in output (should match the major-counts) updated formatting for charater lenght of $1 to 50 Major_Counts_(row_count):"attachments_photos"      BI2R:1134 I2AR:1130 Total_Counts_(pattern_count):"attachments_photos"  BI2R:1134 I2AR:1130

--- major_counts_compare_tool_SP83.sh
+++ major_counts_compare_tool_SP83.sh
@@ -37,16 +37,21 @@ function totalCount () {
         I2AR_INPUT_FILE=$3
         OUTPUT_FILE=$4
         #this is over kill.. gonna change it to just a simple cat $I2AR_INPUT_FILE | wc -l)
-        #on second thought it was useful showing that matchml didn't match
+        #on second thought it was useful showing that matchml didn't match.. total counts using the pattern file passed in to the script.
         I2ARTotal="$(grep -Ff $PATTERN_LIST $I2AR_INPUT_FILE | wc -l)"
         BI2RTotal="$(grep -Ff $PATTERN_LIST $BI2R_INPUT_FILE | wc -l)"
         BASENAME="$(basename $3 .txt)"
+        #row count variables (just a simple row count of BIR and I2AR count files)
+        BI2R_ROW_COUNT_TOTAL="$(cat $BI2R_INPUT_FILE | wc -l)"
+        I2AR_ROW_COUNT_TOTAL="$(cat $I2AR_INPUT_FILE | wc -l)"
 
         #testing awk formatting (not totally nessacary but intresting to learn)
         #echo "Total_Counts:${BASENAME}: BI2R:$BI2RTotal  I2AR:$I2ARTotal" | awk -F" " ' BEGIN { print "============================================================" , printf "%-45s %-9s %-9s\n" $1,$2,$3 , print "============================================================" }' >>  $OUTPUT_FILE
+        # getting the row count from the count files (should correspond to the major count numbers)
+        echo "Major_Counts_(row_count):\"${BASENAME}\" BI2R:$BI2R_ROW_COUNT_TOTAL  I2AR:$I2AR_ROW_COUNT_TOTAL" | awk '{ printf "%-50s %-9s %-9s\n" , $1,$2,$3 }' >>  $OUTPUT_FILE
 
-        #print the first line in output file.. formatting using awk original line below can change spacing with these values "%-45s %-9s %-9s\n"
-        echo "Total_Counts:\"${BASENAME}\" BI2R:$BI2RTotal  I2AR:$I2ARTotal" | awk '{ printf "%-45s %-9s %-9s\n" , $1,$2,$3 }' >>  $OUTPUT_FILE
+        #print the second line in output file.. formatting using awk original line below can change spacing with these values "%-45s %-9s %-9s\n"
+        echo "Total_Counts_(pattern_count):\"${BASENAME}\" BI2R:$BI2RTotal  I2AR:$I2ARTotal" | awk '{ printf "%-50s %-9s %-9s\n" , $1,$2,$3 }' >>  $OUTPUT_FILE
 
 
 }
@@ -72,7 +77,7 @@ function patternCount () {
         PATTERN_TYPE_COUNT_I2AR="$(cat $PATTERN_LIST | sed -n -e $p | xargs -i{} fgrep -i {} $I2AR_INPUT_FILE | wc -l)"
         #adding awk formatting to the output for readablity the original is commented out below:
         #echo "$PATTERN_TYPE BI2R:$PATTERN_TYPE_COUNT_BI2R  I2AR:$PATTERN_TYPE_COUNT_I2AR" >> $OUTPUT_FILE
-        echo "$PATTERN_TYPE BI2R:$PATTERN_TYPE_COUNT_BI2R  I2AR:$PATTERN_TYPE_COUNT_I2AR" | awk '{ printf "%-45s %-9s %-9s\n" , $1,$2,$3 }' >> $OUTPUT_FILE
+        echo "$PATTERN_TYPE BI2R:$PATTERN_TYPE_COUNT_BI2R  I2AR:$PATTERN_TYPE_COUNT_I2AR" | awk '{ printf "%-50s %-9s %-9s\n" , $1,$2,$3 }' >> $OUTPUT_FILE
         ((i=i+1))
         # added just to test the value of echo "$i"
     done
