3bb468c7e6aa7528bf7096aa5752e59397bfeec4
khainguyen24 <35777870+khainguyen24@users.noreply.github.com>
2020-01-06 11:24:00 -0500

    updated help menus and *

--- direct_export_import_count_SP85.sh
+++ direct_export_import_count_SP85.sh
@@ -30,17 +30,20 @@ function display_help_menu() {
 #      Will not work on BI2R Export files that require \"SPLIT\".. note to self add an \"*\" to the ones that require \"SPLIT\"
 #      Use the \"split_file_count.sh\" script for that.
 
-*****************************************************
+\e[32m*****************************************************
 **** APENDIX A - BI2R Export Directory Structure ****
 *****************************************************
 ├── aims
 │   ├── AIM1
 │   └── IMG1
 ├── aims_additional_data
+│   └── aims_additional_data_export.xml *
 ├── atpDetails
+│   └── identity_atp_score_export.xml *
 ├── attachments
 │   └── IMG1
 ├── attributes
+│   └── identity_attribute_export.xml *
 ├── bat_attachments
 │   └── ATT_2_1
 ├── batcxi_attachments
@@ -48,6 +51,7 @@ function display_help_menu() {
 ├── batcxi_photos
 │   └── IMG_JOB3_1
 ├── batcxiRptAnalystNotes
+│   └── batcxi_rpt_analystnotes_export.xml * (not ready for ingest)
 ├── batcxi_rr_attachments
 │   └── attachments
 │       ├── ACT1
@@ -71,6 +75,7 @@ function display_help_menu() {
 ├── bat_photos
 │   └── IMG_1_1
 ├── batRptAnalystNotes
+│   └── bat_rpt_analystnotes_export.xml * (not ready for ingest)
 ├── bat_rr_attachments
 │   └── attachments
 │       ├── ACT1
@@ -89,7 +94,10 @@ function display_help_menu() {
 │   ├── TH_JOB0_1
 │   └── UP1
 ├── bewl
+│   └── bewl_export_data.xml *
 ├── bewl_history
+│   ├── bewl_history.xml *
+│   └── bewl_delete_history_record.xml *
 ├── bi2r-j
 │   └── bi2r-sensors
 │       └── UA1
@@ -113,6 +121,7 @@ function display_help_menu() {
 ├── bids
 ├── datamasking_rules
 ├── deconfliction_trackers
+│   └── deconfliction_tracker_export.xml * (not ready for ingest as of SP85)
 ├── efts_attachments
 │   └── ATT_4_1
 ├── encounters
@@ -127,11 +136,16 @@ function display_help_menu() {
 │   └── EVT1
 ├── export_lists
 ├── harmony
+│   └── harmony_links_data_export.xml *
 ├── identityCrosslinks
 ├── identityHistory
+│   └── identity_history_export.xml * (splitNotReady not ready for ingest as of SP85 see pushCatcher.sh)
 ├── identityMasks
+│   └── identity_mask_data_export.xml *
 ├── identityMerges
+│   └── identity_link_export.xml *
 ├── identityRelationships
+│   └── identity_relationship_export.xml *
 ├── idml
 │   └── IDML1
 ├── link_transactions
@@ -148,8 +162,53 @@ function display_help_menu() {
 │       ├── bi2r-sensors
 │       ├── i2d-sensors
 │       │   └── MF1
+│       └── ss-sensors
+│           └── MF1
+├── misc
+├── nonsensor_matchml
+│   ├── rir
+│   │   └── RIR1
+│   └── uhn
+│       └── UHN1
+├── photos
+│   └── IMG1
+├── primaryName
+│   └── identity_primary_name_export.xml *
+├── primaryPhoto
+│   └── identity_primary_photo_export.xml *
+├── product
+│   └── PROD1
+├── product_attachments
+│   └── PRODATT1
+├── related_reports
+│   ├── AC1
+│   ├── CE1
+│   ├── COM1
+│   ├── EV1
+│   ├── FAC1
+│   ├── INTN1
+│   ├── INTR1
+│   ├── ORG1
+│   ├── PROD1
+│   ├── PROJ1
+│   ├── REQ1
+│   ├── RFI1
+│   ├── RPT1
+│   ├── TECH1
+│   ├── TRNS1
+│   └── TRNSP1
+├── socom
+│   └── socom_export.xml *
+├── sourceComments
+│   └── identity_comments_data_export.xml *
+├── twpdes
+│   └── TWP1
+├── twpdes_deletes
+└── uspTrackerData
+    └── us_person_data.xml *\e[0m
 
-*****************************************************
+
+\e[35m*****************************************************
 **** APENDIX B - I2AR Import Directory Structure ****
 *****************************************************
 ├── aims
@@ -159,11 +218,11 @@ function display_help_menu() {
 │   └── drivers
 │       ├── AIM1
 │       └── IMG1
-├── aims_additional_data
+├── aims_additional_data *
 │   ├── <files within subdirectories>
-├── atpDetails
+├── atpDetails *
 │   ├── <files within subdirectories>
-├── attributes
+├── attributes *
 │   ├── <files within subdirectories>
 ├── bat_attachments
 │   ├── attachments
@@ -288,21 +347,21 @@ function display_help_menu() {
 │   └── ENC1
 ├── events
 │   └── EVT1
-├── harmony
+├── harmony *
 │   └── <files within subdirectories>
-├── identity_comments
+├── identity_comments *
 │   ├── <files within subdirectories>
 ├── identityCrosslinks
 │   ├── <files within subdirectories>
-├── identityEdits-identity_merge
+├── identityEdits-identity_merge *
 │   ├── <files within subdirectories>
-├── identityEdits-identity_relationships
+├── identityEdits-identity_relationships *
 │   ├── <files within subdirectories>
-├── identityMasks
+├── identityMasks *
 │   ├── <files within subdirectories>
-├── identitySetPrimary-name
+├── identitySetPrimary-name *
 │   └── <files within subdirectories>
-├── identitySetPrimary-photo
+├── identitySetPrimary-photo *
 │   └── <files within subdirectories>
 ├── link_transactions
 ├── product
@@ -331,7 +390,7 @@ function display_help_menu() {
 │   └── TRNSP1
 ├── rir
 │   └── RIR1
-├── socom
+├── socom *
 │   ├── <files within subdirectories>
 ├── twpdes
 │   └── TWP1
@@ -348,14 +407,14 @@ function display_help_menu() {
 │   │   └── IMG1
 │   └── drivers
 │       └── IMG1
-├── uspTrackerData
+├── uspTrackerData *
 │   ├── <files within subdirectories>
-├── watchlistEntries-bewl
+├── watchlistEntries-bewl *
 │   └── <files within subdirectories>
-├── watchlistEntries-bewl_history
+├── watchlistEntries-bewl_history *
 │   ├── <files within subdirectories>
-└── watchlistEntries-bewl_history-deletes
-    └── <files within subdirectories>
+└── watchlistEntries-bewl_history-deletes *
+    └── <files within subdirectories>\e[0m
 
 "
 }
